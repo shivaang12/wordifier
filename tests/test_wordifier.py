7f46a283e29e446bc70f4d5cec5d44c1911dc314
Shivang Patel <shivaang14@gmail.com>
2020-04-19 23:38:20 -0400

    Add more tests and modified setup.cfg
    - Increase line lenght for flake8
    
    - Add tests for helper method find_vanity_number_format
    
    - Add more assert statements to the test for wordifier method words_to_number

--- tests/test_wordifier.py
+++ tests/test_wordifier.py
@@ -8,3 +8,6 @@ from wordifier import wordifier
 
 def test_words_to_number():
     assert wordifier.words_to_number("+1-800-PAINTER") == "1-800-724-6837"
+    assert wordifier.words_to_number("800-PAINTER") == "800-724-6837"
+    assert wordifier.words_to_number("800PAINTER") == "800-724-6837"
+    assert wordifier.words_to_number("800-72-HOT-37") == "800-724-6837"
