6965d1b25b47267cae9bf0a31e96cb7cc49d189c
Shivang Patel <shivaang14@gmail.com>
2020-04-20 12:08:42 -0400

    Implemented number-to-words method
    helper:
    - Add find_words method which find all possible combination of valid words
    - Add count_alpha method which is being used as key for sorting the array with highest content for alphabets
    - Add get_alpha_numeric_format method which formats the string which contains alphabets and numbers
    
    indexifier:
    - Add class Indexifier which helps to get the division indexes for substring feature
    
    - Add test for number-to-words method

--- wordifier/helper.py
+++ wordifier/helper.py
@@ -1,4 +1,7 @@
+import itertools
+import re
 import enchant
+from . import indexifier
 
 
 # Dictionary object for checking word in english
@@ -19,8 +22,12 @@ number_to_alpha_dict = {
 }
 
 
+# Indexifier object for the array of substring possiblity combination
+index_division_array = indexifier.Indexifier(7)
+
+
 def find_vanity_number_format(phone_number: str):
-    # TODO(shivaang12): To find some good regex to eliminate this code
+    # TODO(shivaang12): To find some good regex to eliminate next 5 lines of this code
     phone_number = phone_number.replace('+', '')
     phone_number = phone_number.replace(' ', '')
     phone_number = phone_number.replace('(', '')
@@ -34,3 +41,46 @@ def find_vanity_number_format(phone_number: str):
         return prefix+"-", phone_number[len(phone_number)-7:]
     else:
         return ""
+
+
+def find_words(number: str):
+    # We want it to return number if no valid word combination is found
+    # this will allowed to incorporate substring feature like
+    # 72-HOT-37
+    valid_words = [number]
+    word_combination_array = list(itertools.product(*[number_to_alpha_dict[i] for i in number]))
+    word_string_array = [(''.join(x)) for x in word_combination_array]
+
+    for word in word_string_array:
+        if len(word) == 1:
+            # Currently pyenchant returns true for all alphabets and I
+            # think only A and I would make a valid english one letter word
+            if (word == "A" or word == "I"):
+                valid_words.append(word)
+        elif english_dict.check(word):
+            valid_words.append(word)
+
+    return valid_words
+
+
+def count_alpha(number: str):
+    count = 0
+    for char in number:
+        count += 1 if char.isalpha() else None
+    return count
+
+
+def get_alpha_numeric_format(number: str):
+    # This method will format the substring type possibilities
+    # for e.g. 78HOT37 => 78-HOT-37
+    # TODO(shivaang12): May be some room for improvement here.
+    split_str = re.split(r'(\d+)', number)
+    return_str = []
+    for x in range(len(split_str)-1):
+        if len(split_str[x]) > 1 and len(split_str[x+1]) > 1:
+            return_str.append("-")
+        else:
+            return_str.append("")
+            pass
+    array = [j for i in zip(split_str, return_str) for j in i] + [split_str[-1]]
+    return "".join(x for x in array)
--- wordifier/wordifier.py
+++ wordifier/wordifier.py
@@ -2,10 +2,62 @@
 
 
 from . import helper
+import itertools
 
 
 def number_to_words(number: str) -> str:
-    return number
+    """
+    Args:
+    - number: A string representing US vanity phone number.
+              e.g. "+1-800-724-6837", "1-800-724-6837", "8007246837"
+
+    Returns:
+    - str: a string which contains pain digit number.
+              e.g. "1-800-PAINTER", "800-PAINTER"
+
+              currently this function does not support return format
+              such as 800PAINTER. Whatever format you provide in input
+              it will return in "XXX-XXX-XXXX" or "1-XXX-XXX-XXXX" format.
+    """
+    possible_word_list = set()
+
+    formated_number = helper.find_vanity_number_format(number)
+
+    if formated_number == "":
+        print("Invalid Number")
+        return formated_number
+
+    prefix_number, wordify_number = formated_number
+
+    possible_indexes = helper.index_division_array.get_all_possible_indexes()
+
+    # TODO(shivaang12): Should I use while loop?
+    for element in possible_indexes:
+        current_possible_list = [helper.find_words(wordify_number[sub_ele[0]: sub_ele[1]])
+                                 for sub_ele in element]
+        word_string_array = [(''.join(x)) for x in itertools.product(*current_possible_list)]
+
+        for word in word_string_array:
+            possible_word_list.add(word)
+
+        if possible_word_list:
+            # break when find some possible words
+            break
+
+    # Current algorithm allows to generate all digits (this is side effect for supporting
+    # substring combination). We will remove every pure digit string
+    possible_word_list = [x for x in possible_word_list if not x.isdigit()]
+
+    # Sorting the set in terms of more alpha char count so that we get more quality content
+    # in the beginning of the set.
+    possible_word_list = sorted(possible_word_list, key=helper.count_alpha, reverse=True)
+
+    # Now the following operation will format the sting if there is valid substring
+    # for e.g. 72HOT37 => 72-HOT-37 and 7HONDA1 => 7HONDA1 (No change as 1 numeric char only)
+    possible_word_list = [helper.get_alpha_numeric_format(x) for x in possible_word_list]
+
+    # Append the prefix
+    return prefix_number+possible_word_list[0]
 
 
 def words_to_number(number: str) -> str:
